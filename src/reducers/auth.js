bc732692d15e5f6250dfd1acd9881bdd0f527983
seabelis <seabelis74@gmail.com>
2019-11-05 20:34:15 +0100

    logout works

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,12 @@
-import { USER_LOGIN } from "../actions/auth";
+import { USER_LOGIN, USER_LOGOUT } from "../actions/auth";
 
 export default function user(state = null, action = {}) {
   switch (action.type) {
     case USER_LOGIN:
       return action.payload;
-
+      case USER_LOGOUT:
+        localStorage.clear();
+        return action.payload;
     default:
       return state;
   }
